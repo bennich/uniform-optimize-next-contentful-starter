f6e3488e4823d941daddc54a826bdd7ef8bbebd5
Christian Bennich <christianb@uniform.dev>
2021-06-09 10:17:29 +0200

    changed form to support intents

--- components/NewsLetterForm.tsx
+++ components/NewsLetterForm.tsx
@@ -34,13 +34,13 @@ export const NewsLetterForm = () => {
     <label>Your Email: <input type="email" name="email" /></label>
   </p>
   <p>
-    <label>Your Role: <select name="role[]" multiple>
-        <option value="leader">Leader</option>
-        <option value="follower">Follower</option>
-      </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message" defaultValue={""} /></label>
+    <label>I want to hear more about: 
+      <select name="interest">
+        <option value="cpf">I'm interested in speaking</option>
+        <option value="marketer">Marketing sessions</option>
+        <option value="dev">Developer sessions</option>
+      </select>
+    </label>
   </p>
   <p>
     <button type="submit">Send</button>
