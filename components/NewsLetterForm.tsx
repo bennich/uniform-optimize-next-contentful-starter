0c1446637550f4569b26e1a61c64a34ce06a3a70
Christian Bennich <christianb@uniform.dev>
2021-06-10 15:26:07 +0200

    added GA event track

--- components/NewsLetterForm.tsx
+++ components/NewsLetterForm.tsx
@@ -1,53 +1,52 @@
 import React from 'react';
 import { useUniformTracker } from '@uniformdev/optimize-tracker-react';
+import {analytics} from '../lib/local-tracker' 
 
 export const NewsLetterForm = () => {
 
   const { tracker } = useUniformTracker();
 
-  // const OnEnrichmentClick = async (EnrichmentLanguage) => {
-  //     const tracked = await tracker.addEnrichment({
-  //       name: 'Language',
-  //       value: EnrichmentLanguage,
-  //     })
-      
-  //     console.log(tracked);
-  // };
+   const OnFormSubmit = async (interest: string) => {
+     const eventLabel = 'Newsletter submission';
+     const eventCategory = 'Forms';
+     const eventvalue = interest;
 
-  // const OnEnrichmentClickFireEvent = async (event: string) => {
-  //   const eventFired = await tracker?.addEvent({
-  //       label: 'EventTestLabel', 
-  //       category: 'EventTestCategory',
-  //       value: 'EventTestValue',
-  //   })
-  // };
+     const eventFired = await tracker?.addEvent({
+         label: eventLabel, 
+         category: eventCategory,
+         value: eventvalue,
+     })
+
+     analytics.track('Newsletter submission');
+
+   };
 
 
     return (
       <>
-
-<form name="contact" method="POST" data-netlify="true" content-type="application/x-www-form-urlencoded" action="/">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>I want to hear more about: 
-      <select name="interest">
-        <option value="cpf">I'm interested in speaking</option>
-        <option value="marketer">Marketing sessions</option>
-        <option value="dev">Developer sessions</option>
-      </select>
-    </label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-  <input type="hidden" name="form-name" value="contact" />
-</form>
-
+        <div>
+          <form name="newsletter" method="POST" data-netlify="true" content-type="application/x-www-form-urlencoded" action="/" onSubmit={ () => OnFormSubmit('Typescript')}>
+            <p>
+              <label>Your Name: <input type="text" name="name" /></label>   
+            </p>
+            <p>
+              <label>Your Email: <input type="email" name="email" /></label>
+            </p>
+            <p>
+              <label>I want to hear more about: 
+                <select name="interest">
+                  <option value="cpf">I'm interested in speaking</option>
+                  <option value="marketer">Marketing sessions</option>
+                  <option value="dev">Developer sessions</option>
+                </select>
+              </label>
+            </p>
+            <p>
+              <button type="submit">Send</button>
+            </p>
+            <input type="hidden" name="form-name" value="newsletter" />
+          </form>
+        </div>
     </>
   );
 };
